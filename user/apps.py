b51abc672e4e975647e6a0e0390166a9f34df869
Roman Harmatiuk <roman.harmatiuk@gmail.com>
2023-11-01 15:29:54 +0200

    register user and userprofile models in admin.py

--- user/apps.py
+++ user/apps.py
@@ -2,5 +2,10 @@ from django.apps import AppConfig
 
 
 class UserConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'user'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "user"
+
+
+class UserProfileConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "user"
