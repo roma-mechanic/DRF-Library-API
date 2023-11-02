60f3b0f070b51150e55776d93f38dc50b65dcfe7
Roman Harmatiuk <roman.harmatiuk@gmail.com>
2023-11-02 19:58:49 +0200

    create borrowing model and migrate.

--- borrowing/models.py
+++ borrowing/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+from book.models import Book
+from user.models import UserProfile
+
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField(auto_now_add=True)
+    expected_return_date = models.DateField(auto_now_add=True)
+    actual_return_date = models.DateField(auto_now_add=True)
+    book = models.ManyToManyField(Book, related_name="borrowing")
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name="borrowing")
