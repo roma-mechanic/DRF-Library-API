cdca21629d6fdf11ffd72fcfabd47ac1ec3fc535
Roman Harmatiuk <roman.harmatiuk@gmail.com>
2023-11-02 12:47:09 +0200

    create book model

--- book/models.py
+++ book/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    class CoverChoices(models.TextChoices):
+        HARD = "hard"
+        SOFT = "soft"
+
+    title = models.CharField(
+        max_length=100,
+    )
+    author = models.CharField(max_length=100)
+    cover = models.CharField(max_length=4, choices=CoverChoices.choices)
+    inventory = models.PositiveIntegerField()
+    daily_fee = models.DecimalField(max_digits=5, decimal_places=2)
