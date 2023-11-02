38dbaef47bc2ef4a56895aa267a2cb58c74873fe
Roman Harmatiuk <roman.harmatiuk@gmail.com>
2023-11-02 20:21:46 +0200

    added to borrowingg model ordering by borrow_date.

--- borrowing/models.py
+++ borrowing/models.py
@@ -10,3 +10,7 @@ class Borrowing(models.Model):
     actual_return_date = models.DateField(auto_now_add=True)
     book = models.ManyToManyField(Book, related_name="borrowing")
     user = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name="borrowing")
+
+    class Meta:
+        ordering = ("-borrow_date",)
+
