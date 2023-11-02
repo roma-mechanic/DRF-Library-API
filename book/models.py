053eca594fa501a9fe6912fbb205baec803cb70a
Roman Harmatiuk <roman.harmatiuk@gmail.com>
2023-11-02 13:10:23 +0200

    create book serializer

--- book/models.py
+++ book/models.py
@@ -1,4 +1,15 @@
+import os
+import uuid
+
 from django.db import models
+from django.utils.text import slugify
+
+
+def movie_image_file_path(instance, filename):
+    _, extension = os.path.splitext(filename)
+    filename = f"{slugify(instance.title)}-{uuid.uuid4()}{extension}"
+
+    return os.path.join("uploads/post", filename)
 
 
 class Book(models.Model):
@@ -10,6 +21,8 @@ class Book(models.Model):
         max_length=100,
     )
     author = models.CharField(max_length=100)
+    annotation = models.TextField
     cover = models.CharField(max_length=4, choices=CoverChoices.choices)
     inventory = models.PositiveIntegerField()
     daily_fee = models.DecimalField(max_digits=5, decimal_places=2)
+    image = models.ImageField(null=True, upload_to=movie_image_file_path)
