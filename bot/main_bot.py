b4a394b2215c9b579a26476cfe5038beb1288a8a
Roman Harmatiuk <roman.harmatiuk@gmail.com>
2023-12-05 20:39:30 +0200

    created helper for sent message to bot.

--- bot/main_bot.py
+++ bot/main_bot.py
@@ -1,7 +1,11 @@
 import telebot
 from django.conf import settings
+from telebot.types import Update
+import requests
 
 API_TOKEN = settings.TELEGRAM_TOKEN
+BOT_CHAT_ID = settings.BOT_CHAT_ID
+
 
 bot = telebot.TeleBot(API_TOKEN)
 
@@ -21,4 +25,20 @@ I am here to echo your kind words back to you. Just say anything nice and I'll s
 # Handle all other messages with content_type 'text' (content_types defaults to ['text'])
 @bot.message_handler(func=lambda message: True)
 def echo_message(message):
-    bot.reply_to(message, message.text)
+    reply_text = f"Your ID = {message.chat.id}.\n {message.text}\n Це маячня. Введіть притомніший текст"
+    bot.reply_to(message, reply_text)
+
+
+def telegram_bot_sendtext(bot_message):
+    send_text = (
+        "https://api.telegram.org/bot"
+        + API_TOKEN
+        + "/sendMessage?chat_id="
+        + BOT_CHAT_ID
+        + "&parse_mode=Markdown&text="
+        + bot_message
+    )
+
+    response = requests.get(send_text)
+
+    return response.json()
